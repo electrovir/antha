ec677798b7b8c9e68f0f85e3676d83aebf876ad1
electrovir <electrovir@users.noreply.github.com>
2026-02-27 19:57:03 +0000

    add github link

--- packages/demo/src/data/all-demos.ts
+++ packages/demo/src/data/all-demos.ts
@@ -2,7 +2,9 @@ import {arrayToObject} from '@augment-vir/common';
 import {type AnthaDemo} from './demo.js';
 import {basicEngineDemo} from './demos/basic-engine.demo.js';
 
-export const allDemos: AnthaDemo[] = [basicEngineDemo];
+export const allDemos: AnthaDemo[] = [
+    basicEngineDemo,
+];
 
 export const allDemosByPathKey = arrayToObject(allDemos, (demo) => {
     return {
--- packages/demo/src/ui/elements/antha-demo-picker.element.ts
+++ packages/demo/src/ui/elements/antha-demo-picker.element.ts
@@ -1,6 +1,6 @@
 import {toSimpleDatePartString, toTimestamp} from 'date-vir';
 import {css, defineElement, html} from 'element-vir';
-import {ViraLink} from 'vira';
+import {ExternalLink24Icon, noNativeSpacing, ViraIcon, ViraLink} from 'vira';
 import {allDemos} from '../../data/all-demos.js';
 import {demoPathTree, type DemoRouter} from '../../data/demo-router.js';
 
@@ -13,17 +13,34 @@ export const AnthaDemoPicker = defineElement<{
             display: flex;
             flex-direction: column;
             padding: 32px;
+            gap: 32px;
             font-family: sans-serif;
         }
 
         ol {
+            ${noNativeSpacing}
+            padding-left: 1em;
             display: flex;
             flex-direction: column;
+        }
+
+        ${ViraLink} {
+            > span {
+                display: flex;
+                align-items: center;
+                gap: 4px;
+            }
+
+            ${ViraIcon} {
+                margin-top: -2px;
+                height: 20px;
+                aspect-ratio: 1;
+            }
         }
     `,
     render({inputs}) {
         const demos = allDemos.toSorted(
-            (a, b) => toTimestamp(a.sortDate) - toTimestamp(b.sortDate),
+            (a, b) => toTimestamp(b.sortDate) - toTimestamp(a.sortDate),
         );
 
         if (!demos.length) {
@@ -51,9 +68,23 @@ export const AnthaDemoPicker = defineElement<{
         });
 
         return html`
-            <ol>
+            <ol reversed>
                 ${listTemplates}
             </ol>
+            <${ViraLink.assign({
+                link: {
+                    url: 'https://github.com/electrovir/antha',
+                    newTab: true,
+                },
+            })}>
+                <span>
+                    <span>GitHub</span>
+                    <${ViraIcon.assign({
+                        icon: ExternalLink24Icon,
+                        fitContainer: true,
+                    })}></${ViraIcon}>
+                </span>
+            </${ViraLink}>
         `;
     },
 });
